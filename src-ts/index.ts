14d12523fa0f8eee0427995dd95da9182bd50069
Forrest <forrest>
2020-11-30 17:47:10 -0500

    update

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -9,13 +9,31 @@ export const Handler = async (
   ): Promise<APIGatewayProxyResult> => {
     
     const queries = JSON.stringify(event.queryStringParameters);
-    const span = tracer.startSpan('Test Span');
-    span.setAttribute("body", event.body);
-    span.setAttribute("headers",event.headers)
-    span.end();
-
+    const span = tracer.startSpan('handler');
+    tracer.withSpan(span, ()=>{
+      someWork();
+      span.setAttribute("body", event.body);
+      span.setAttribute("headers",event.headers)
+      span.end();
+    })
     return {
       statusCode: 200,
       body: `A working typescript Lambda response`
     }
   }
+
+
+
+const someWork= () =>{
+  const span = tracer.startSpan('someWork');
+  tracer.withSpan(span, ()=>{
+    moreWork()
+    span.end()
+  })
+}
+
+const moreWork= () =>{
+  const span = tracer.startSpan('moreWork');
+  span.end()
+}
+
